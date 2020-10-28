a33977c59419bc32d799f77955c0fee3d641b640
Gustavo Quintana <gustavo.quintana1@davinci.edu.ar>
2020-10-27 22:42:38 -0300

    modificaciones Mariano - counter

---
+++ cards-commers/.history/src/Components/ItemCount/ViewCount/ViewCount_20201027223903.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const countnumber = {
+    margin:"0.35em auto",
+    fontStyle:"italic",
+
+}
+
+export default function ViewCount({value}){
+ 
+    return(
+        <div style={countnumber} class="countNumber">{value}</div>
+    )
+}
